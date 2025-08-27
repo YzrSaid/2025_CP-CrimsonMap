b297ba649951bb3b0689132c3eebc3c40c2d10ac
Aldrin Said <said.mohammadaldrin.2021@gmail.com>
2025-08-27 14:26:40 +0800

    ADDED: Dropdown_To and Dropdown_From populate from BuildingData

---
+++ Assets/Scripts/Main App Scripts/Buildings.cs
@@ -0,0 +1,15 @@
+// Buildings data
+
+[System.Serializable]
+public class Buildings
+{
+    public int building_id;
+    public string name;
+    public int category_id;
+    public string location;
+    public float latitue;
+    public float longitude;
+    public string image_url;
+    public string email;
+    public string phone;
+}
